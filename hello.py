f7f0936aea864a3b72353eed1a2c79e57e366220
Dump Ma <poppular105@hotmail.com>
2017-10-21 14:24:32 +0700

    modified file

--- hello.py
+++ hello.py
@@ -2,4 +2,16 @@ def main():
     num = 0
     while True:
         print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
+        print("sopoat hui kuy!!!!!")
 main()
