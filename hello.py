e81f692593958bcf18819e55d7b2b89ba6e28b3e
Sopoat Iamcharoen <60070101@kmitl.ac.th>
2017-10-21 14:24:14 +0700

    Loop is True

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 def main():
     num = 0
     while True:
-        print("sopoat hui kuy!!!!!")
+        print("Oh Dump!!!!!")
+    eiei = 1
+    yoyo = 0
+    while True:
+        eiei = eiei + eiei
+        print(eiei)
 main()
